563d459514af4d5f2d21f348fc383d97e405265e
amiroudiallo03 <amiroudiallo03@gmail.com>
2022-02-11 14:11:29 +0000

    payment

--- cagnote_app/urls.py
+++ cagnote_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.api_academiciens),
     path('reasons/', views.ReasonsAPIView.as_view()),
-    path('reasons/<int:pk>', views.ReasonAPIView.as_view())
+    path('reasons/<int:pk>', views.ReasonAPIView.as_view()),
     path('academicians', views.api_academiciens),
     path('reasons/', views.ReasonsAPIView.as_view()),
     path('academicians/<str:register_number>', views.api_academician),
