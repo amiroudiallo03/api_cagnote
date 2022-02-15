d86a6e350c65aca88f9aadcad7d01b6deca444ad
HackerMan018 <bouadoutanoh9@gmail.com>
2022-02-15 09:00:29 +0000

    deploiement

--- api/settings.py
+++ api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -119,7 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/media/'
 
+STATIC_ROOT = BASE_DIR / 'static_cdn'
+
+MEDIA_ROOT = BASE_DIR / 'media_cdn'
+
+STATICFILES_DIRS = [BASE_DIR /'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
